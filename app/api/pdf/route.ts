b4217fdb7a6bed86104563e88d652fdc7d19a9b0
Diego Peter <dpl@cesar.school>
2024-02-20 22:45:30 -0300

    new fix

--- app/api/pdf/route.ts
+++ app/api/pdf/route.ts
@@ -1,81 +1,88 @@
 import { CVProps } from "@/types/cv-types";
 import { encode } from 'urlencode';
-import puppeteer from "puppeteer";
-import chromium from 'chrome-aws-lambda';
-import playwright from 'playwright-core';
-
-// export async function POST(request: Request): Promise<Response> {
-//     const data = await request.json() as CVProps
-//     console.log()
-//     const cvData = encode(JSON.stringify(data))
-//     const browser = await puppeteer.launch()
-//     const page = await browser.newPage()
-
-//     //TODO: change the url to the actual url of the cv_only page
-//     if (process.env.NODE_ENV === 'development') {
-//         await page.goto(`http://localhost:3000/cv_only/${cvData}`)
-//     } else {
-//         await page.goto(`https://easycv-jet.vercel.app/cv_only/${cvData}`)
-//     }
-//     // await page.goto(`http://localhost:3000/cv_only/${cvData}`)
-//     await page.emulateMediaType('screen')
-//     const pdfBuffer = await page.pdf({format: 'A4'})
-//     await browser.close()
-//     return new Response(pdfBuffer, {
-//         headers: {
-//             'Content-Type': 'application/pdf',
-//             'Content-Disposition': `attachment; filename="${data.header.firstName}-EasyCV.pdf"`
-//         }
-//     })
-// }
+import puppeteer from "puppeteer-core";
+import chromium from '@sparticuz/chromium'
+// import chromium from 'chrome-aws-lambda';
+// import playwright from 'playwright-core';
 
 export async function POST(request: Request): Promise<Response> {
-    try {
-        const data = await request.json() as CVProps
-        console.log()
-        const cvData = encode(JSON.stringify(data))
+    const data = await request.json() as CVProps
+    console.log()
+    const cvData = encode(JSON.stringify(data))
+    chromium.setGraphicsMode = false
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless === true ? 'shell': true,
+    })
+    const page = await browser.newPage()
 
-        const browser = await playwright.chromium.launch({
-            args: [...chromium.args, "--font-render-hinting=none"],
-            executablePath:
-                process.env.NODE_ENV === 'production'
-                    ? await chromium.executablePath
-                    : "/usr/local/bin/chromium",
-                headless:
-                process.env.NODE_ENV === 'production'
-                    ? chromium.headless
-                    : true,
-        })
-        const context = await browser.newContext()
-        const page = await context.newPage()
-
-        const pdfUrl = process.env.NODE_ENV === 'production'
-        ? `https://easycv-jet.vercel.app/cv_only/${cvData}`
-        : `http://localhost:3000/cv_only/${cvData}`
-
-        await page.goto(pdfUrl, {
-            waitUntil: 'load'
-        })
-
-        const pdf = await page.pdf({
-            path: `/tmp/${data.header.firstName}-EasyCV.pdf`,
-            printBackground: true,
-            format: 'A4',
-        })
-        await browser.close()
-        return new Response(pdf, {
-            headers: {
-                'Content-Type': 'application/pdf',
-                'Content-Disposition': `attachment; filename="${data.header.firstName}-EasyCV.pdf"`
-            },
-            status: 200
-        })
-    } catch (e) {
-        console.error(e)
-        return new Response('Internal Server Error',{
-            status: 500
-        })
+    //TODO: change the url to the actual url of the cv_only page
+    if (process.env.NODE_ENV === 'development') {
+        await page.goto(`http://localhost:3000/cv_only/${cvData}`)
+    } else {
+        await page.goto(`https://easycv-jet.vercel.app/cv_only/${cvData}`)
     }
-    
+    // await page.goto(`http://localhost:3000/cv_only/${cvData}`)
+    await page.emulateMediaType('screen')
+    const pdfBuffer = await page.pdf({format: 'A4'})
+    await browser.close()
+    return new Response(pdfBuffer, {
+        headers: {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': `attachment; filename="${data.header.firstName}-EasyCV.pdf"`
+        }
+    })
 }
+
+// export async function POST(request: Request): Promise<Response> {
+//     try {
+//         const data = await request.json() as CVProps
+//         console.log()
+//         const cvData = encode(JSON.stringify(data))
+
+//         const browser = await playwright.chromium.launch({
+//             args: [...chromium.args, "--font-render-hinting=none"],
+//             executablePath:
+//                 process.env.NODE_ENV === 'production'
+//                     ? await chromium.executablePath
+//                     : "/usr/local/bin/chromium",
+//                 headless:
+//                 process.env.NODE_ENV === 'production'
+//                     ? chromium.headless
+//                     : true,
+//         })
+//         const context = await browser.newContext()
+//         const page = await context.newPage()
+
+//         const pdfUrl = process.env.NODE_ENV === 'production'
+//         ? `https://easycv-jet.vercel.app/cv_only/${cvData}`
+//         : `http://localhost:3000/cv_only/${cvData}`
+
+//         await page.goto(pdfUrl, {
+//             waitUntil: 'load'
+//         })
+
+//         const pdf = await page.pdf({
+//             path: `/tmp/${data.header.firstName}-EasyCV.pdf`,
+//             printBackground: true,
+//             format: 'A4',
+//         })
+//         await browser.close()
+//         return new Response(pdf, {
+//             headers: {
+//                 'Content-Type': 'application/pdf',
+//                 'Content-Disposition': `attachment; filename="${data.header.firstName}-EasyCV.pdf"`
+//             },
+//             status: 200
+//         })
+//     } catch (e) {
+//         console.error(e)
+//         return new Response('Internal Server Error',{
+//             status: 500
+//         })
+//     }
+    
+// }
 
