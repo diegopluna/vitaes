b7f4a1a641c6910979af532ed952bba87c0221db
Diego Peter <dpl@cesar.school>
2024-02-23 23:44:17 -0300

    Commented out DrizzleAdapter in auth.ts while the issue is not fixed

--- auth.ts
+++ auth.ts
@@ -4,6 +4,6 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import db from "@/db/drizzle";
 
 export const { handlers, auth, signOut } = NextAuth({
-    adapter: DrizzleAdapter(db),
+    // adapter: DrizzleAdapter(db),
     providers: [Github]
 });
