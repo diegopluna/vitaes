e7178954c900d5cb079f758278e8f48012f65025
Diego Peter <diegopeter9@gmail.com>
2025-05-22 23:44:33 -0300

    another quickfix

--- app/[locale]/builder/_components/resume/form/summary-form.tsx
+++ app/[locale]/builder/_components/resume/form/summary-form.tsx
@@ -6,13 +6,14 @@ import { useResumeStore } from '@/providers/resume-store-provider'
 export const SummaryForm = () => {
   const { resume, setResumeField } = useResumeStore(s => s)
   const summary = resume.basics.summary || { label: 'Summary', content: '' }
+  const content = typeof summary.content === 'string' ? summary.content : ''
 
   return (
     <div className="flex px-2">
       <div className="w-full">
         <Textarea
           id="summary"
-          value={summary.content}
+          value={content}
           onChange={e =>
             setResumeField('basics', {
               ...resume.basics,
