1df4642fb3deeafa90ad1e4432e3b7e632c570c5
Diego Peter <dpeter@dpeter.dev>
2025-09-01 18:19:58 -0300

    Remove everything

--- src/components/resume/a4-resume-only.tsx
+++
@@ -1,7 +0,0 @@
-export function A4ResumeOnly({ children }: { children: React.ReactNode }) {
-	return (
-		<div className=" h-min-[297mm] w-[210mm] pr-[14mm] pl-[14mm] bg-white">
-			{children}
-		</div>
-	)
-}
--- src/db/index.ts
+++
@@ -1,5 +0,0 @@
-import { env } from "@/env/server";
-import { drizzle as neon } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-
-export const db = neon(env.DATABASE_URL, { schema });
