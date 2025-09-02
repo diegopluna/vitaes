f6de1ef417880b0b3c5eaec143f6b97af432de23
Diego Peter <dpeter@dpeter.dev>
2025-09-02 12:02:49 -0300

    feat: implement SEO utility and update metadata; add environment configuration files

---
+++ env/server.ts
@@ -0,0 +1,13 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+ 
+export const env = createEnv({
+  server: {
+  },
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  // runtimeEnv: {
+  //   DATABASE_URL: process.env.DATABASE_URL,
+  //   OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+  // },
+  // For Next.js >= 13.4.4, you can just reference process.env:
+  experimental__runtimeEnv: process.env
+});
