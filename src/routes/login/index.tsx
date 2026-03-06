7c395f4f4dbde8b331622093e7dc42e43525e29b
Diego Luna <dpeter@dpeter.dev>
2026-03-06 14:49:07 -0300

    feat: add Instrument Sans font and update styles for improved typography

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -16,11 +16,11 @@ function RouteComponent() {
       <div className="bg-[#111111] hidden lg:flex h-full w-5/8 p-15 items-start flex-col justify-between">
         <div className="flex flex-row gap-3 items-center">
           <img src="/logo.svg" className="size-12" />
-          <span className="font-bold text-[22px]">Vitaes</span>
+          <span className="font-bold font-heading text-[22px]">Vitaes</span>
         </div>
 
         <div className="items-start flex flex-col gap-8">
-          <span className="font-bold text-[44px]">
+          <span className="font-bold font-heading text-[44px]">
             Your carrer story,
             <br />
             perfectly told.
@@ -32,9 +32,9 @@ function RouteComponent() {
           </span>
 
           <div className="flex flex-col items-start gap-2">
-            <div className="h-1 w-72 bg-primary" />
-            <div className="h-1 w-48 bg-primary opacity-50" />
-            <div className="h-1 w-36 bg-primary opacity-25" />
+            <div className="h-1 w-70 bg-primary" />
+            <div className="h-1 w-50 bg-primary opacity-50" />
+            <div className="h-1 w-35 bg-primary opacity-25" />
           </div>
         </div>
 
@@ -43,12 +43,17 @@ function RouteComponent() {
       <div className="h-full w-full lg:w-3/8 flex flex-col justify-between p-15 items-center">
         <div className="flex flex-row gap-2.5 items-center">
           <img src="/logo.svg" className="size-9 lg:hidden" />
-          <span className="font-bold text-[22px] lg:hidden">Vitaes</span>
+          <span className="font-bold text-[22px] lg:hidden font-heading">
+            Vitaes
+          </span>
         </div>
 
         <div className="flex flex-col items-center gap-8 w-full">
+          <div className="h-0.75 w-10 bg-primary" />
           <div className="flex flex-col items-center gap-3">
-            <span className="font-bold text-[32px]">Welcome back</span>
+            <span className="font-bold text-[32px] font-heading">
+              Welcome back
+            </span>
             <span className="text-muted-foreground text-[14px]">
               Sign in to continue building your resume
             </span>
@@ -63,14 +68,14 @@ function RouteComponent() {
             </Button>
             <Button className="w-full" variant="secondary" size="lg">
               <IconBrandGithubFilled className="size-5.5 mr-2.5" />
-              Continue with Google
+              Continue with GitHub
             </Button>
           </div>
         </div>
 
         <Link
           to="/"
-          className="flex flex-row items-center gap-1.5 text-primary text-[13px]"
+          className="flex flex-row items-center gap-1.5 text-primary text-[13px] hover:underline"
         >
           <IconArrowLeft className="size-3.5" />
           Back to home
